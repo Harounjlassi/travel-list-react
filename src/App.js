6bc1fe6ca47fec4de5fd47fe724818c122fc1e32
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-06 15:36:47 +0100

    style the picked item

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Socks", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -31,13 +37,22 @@ function PackingList() {
   return (
     <div className="list">
       <ul>
-        <li>
-          <input type="checkbox" id="demoCheckbox" name="checkbox" value="1" />
-          <label for="demoCheckbox"> Check me!</label>
-          <button>X</button>
-        </li>
+        {initialItems.map((i) => (
+          <Item item={i} />
+        ))}
       </ul>
     </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
 function Status() {
