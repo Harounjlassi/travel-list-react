2e989d12dfb318b0903634a148c204a9af3ae031
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-07 12:08:19 +0100

    add controlled elments

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -18,19 +20,41 @@ function Logo() {
   return <h1> 🍀 Far Away🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  console.log(description);
+  function handleSubmit(event) {
+    console.log(event);
+    event.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={(e) => handleSubmit(e)}>
       <h3>what do you need for your 😍 trip?</h3>
-      <select>
-        <option value="actual value 1">1</option>
-        <option value="actual value 1">2</option>
-        <option value="actual value 2">3</option>
-        <option value="actual value 1">4</option>
-        <option value="actual value 3">5</option>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          console.log(e.target);
+          setQuantity(Number(e.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
-      <input placeholder="item ..."></input>
+      <input
+        type="text"
+        placeholder="Item ..."
+        value={description}
+        onChange={(e) => {
+          console.log(e.target);
+          //<input type="text" placeholder="Item ..." value="d">: console.log(e.target);
+          setDescription(e.target.value);
+        }}
+      ></input>
       <button>ADD</button>
-    </div>
+    </form>
   );
 }
 function PackingList() {
@@ -38,7 +62,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((i) => (
-          <Item item={i} />
+          <Item item={i} key={i.id} />
         ))}
       </ul>
     </div>
@@ -58,7 +82,7 @@ function Item({ item }) {
 function Status() {
   return (
     <footer className="stats">
-      <em> 🧳YOu have 3 itmes on your tist, and you already packed X</em>
+      <em> 🧳You have 3 itmes on your tist, and you already packed X</em>
     </footer>
   );
 }
