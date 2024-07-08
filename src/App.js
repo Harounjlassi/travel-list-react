07cf93b6e95e73fc27a879938bc551c18d99e505
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-08 11:23:57 +0100

    customise the footer

--- src/App.js
+++ src/App.js
@@ -121,15 +121,27 @@ function Item({ item, onDeleteItem, onhandleToggleItem }) {
   );
 }
 function Status({ items }) {
+  if (items.length == 0)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to packing list 🚀</em>
+      </footer>
+    );
+
   //derived state
   const numItems = items.length;
   const itemPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numItems / itemPacked) * 100);
+  console.log(numItems);
+  console.log(itemPacked);
+  console.log(percentage);
   return (
     <footer className="stats">
       <em>
-        {" "}
-        🧳You have {numItems} itmes on your tist, and you already packed{" "}
-        {itemPacked}
+        {percentage == 100
+          ? "You have everything Ready to go ✈"
+          : `🧳You have ${numItems} itmes in your list, and you already packed
+        ${itemPacked} ( ${isFinite(percentage) ? percentage : 0}%)`}
       </em>
     </footer>
   );
